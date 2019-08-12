6fa3c7c3c83ed04ad5584127735aca6487555d26
stefandzaleski <stefan.dzaleski@gmail.com>
2019-08-12 15:34:02 +0200

    Updates

---
+++ precache-manifest.c8f12d90ba11c68f2a49b61275cea058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82252091cc0cecb39b7b40ed5a5cad7",
+    "url": "/nasdaq-stock-visualisation/index.html"
+  },
+  {
+    "revision": "a6bf65a2652f31d7be0a",
+    "url": "/nasdaq-stock-visualisation/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "1aa76e3a2cbfe24a06b6",
+    "url": "/nasdaq-stock-visualisation/static/css/main.290edff6.chunk.css"
+  },
+  {
+    "revision": "a6bf65a2652f31d7be0a",
+    "url": "/nasdaq-stock-visualisation/static/js/2.f3277bf4.chunk.js"
+  },
+  {
+    "revision": "1aa76e3a2cbfe24a06b6",
+    "url": "/nasdaq-stock-visualisation/static/js/main.1c749c72.chunk.js"
+  },
+  {
+    "revision": "a2fc3747bfb05cc62c3c",
+    "url": "/nasdaq-stock-visualisation/static/js/runtime~main.e8e355f0.js"
+  }
+]);
